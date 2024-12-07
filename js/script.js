a6edf84df3a91f6b274cdbe9ca81c036808be360
Oleksandr Pavlishchev <velogitara@gmail.com>
2024-12-07 23:29:40 +0400

    add more tasks

--- js/script.js
+++ js/script.js
@@ -225,22 +225,132 @@
 // console.log(getEvenNumbers(7, 7))
 // console.log(getEvenNumbers(8, 8))
 
-function includes(array, value) {
+// function includes(array, value) {
+//     // Change code below this line
+//     for (const number of array) {
+//         if (number === value) {
+//             return true;
+//         }
+//         // Change code above this line
+//     }
+//     return false;
+// }
+
+// console.log(includes([1, 2, 3, 4, 5], 3));
+// console.log(includes([1, 2, 3, 4, 5], 17));
+// console.log(
+//     includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter")
+// );
+// console.log(
+//     includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus")
+// );
+
+// const apartment = {
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Change code below this line
+// for (const key in apartment) {
+//     if (apartment.hasOwnProperty(key)) {
+//         keys.push(key);
+//         values.push(apartment[key]);
+//     }
+// }
+// console.log(keys, values);
+// console.log(apartment);
+
+// function countProps(object) {
+//     let propCount = 0;
+//     // Change code below this line
+//     for (const key in object) {
+//         if (object.hasOwnProperty(key)) {
+//             propCount += 1;
+//         }
+//         console.log(propCount);
+//     }
+//     // Change code above this line
+//     return propCount;
+// }
+
+// console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
+
+// const apartment = {
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// for (const key of keys) {
+//     values.push(apartment[key]);
+// }
+
+// function countTotalSalary(salaries) {
+//     let totalSalary = 0;
+//     // Change code below this line
+//     const values = Object.values(salaries);
+//     for (let value of values) {
+//         totalSalary += value;
+//     }
+
+//     // Change code above this line
+//     return totalSalary;
+// }
+
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+
+// const colors = [
+//     { hex: "#f44336", rgb: "244,67,54" },
+//     { hex: "#2196f3", rgb: "33,150,243" },
+//     { hex: "#4caf50", rgb: "76,175,80" },
+//     { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+// for (const color of colors) {
+//     hexColors.push(color.hex);
+//     rgbColors.push(color.rgb);
+// }
+
+const products = [
+    { name: "Radar", price: 1300, quantity: 4 },
+    { name: "Scanner", price: 2700, quantity: 3 },
+    { name: "Droid", price: 400, quantity: 7 },
+    { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getProductPrice(productName) {
     // Change code below this line
-    for (const number of array) {
-        if (number === value) {
-            return true;
+    for (const { name, price } of products) {
+        if (name === productName) {
+            return price;
         }
-        // Change code above this line
     }
-    return false;
+    return null;
+
+    // Change code above this line
 }
 
-console.log(includes([1, 2, 3, 4, 5], 3));
-console.log(includes([1, 2, 3, 4, 5], 17));
-console.log(
-    includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter")
-);
-console.log(
-    includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus")
-);
+function getAllPropValues(propName) {
+    // Change code below this line
+    let newArrayKeyName = [];
+    for (const product of products) {
+        if (Object.keys(product).includes(propName)) {
+            newArrayKeyName.push(product[propName]);
+        }
+
+    }
+    return newArrayKeyName;
+    // Change code above this line
+}
+
+console.log(getAllPropValues("name"));
+console.log(getAllPropValues("quantity"));
+console.log(getAllPropValues("category"));
